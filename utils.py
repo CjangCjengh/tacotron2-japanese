9b3b0a64125a7be04afab365d1c4d2afc57a8dda
Watermelon <62407841+StarxSky@users.noreply.github.com>
2022-08-15 21:18:29 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,11 +4,11 @@ import torch
 
 
 from hparams import create_hparams
-hparams = create_hparams()
+hparam = create_hparams()
 
 def get_mask_from_lengths(lengths):
     max_len = torch.max(lengths).item()
-    if hparams.cuda_enabled :
+    if hparam.cuda_enabled :
         ids = torch.arange(0, max_len, out=torch.cuda.LongTensor(max_len))
         mask = (ids < lengths.unsqueeze(1)).bool()
     else :
